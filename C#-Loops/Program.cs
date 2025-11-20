a3004717c212e327a69d853d14e2aa5527dc8567
Lonatri <gyda-94@hotmail.com>
2025-11-20 11:29:49 +0100

    Add project files.

---
+++ C#-Loops/Program.cs
@@ -0,0 +1,10 @@
+﻿
+ForLoop();
+static void ForLoop()
+{
+    for(int i = 0; i<5; i++)
+    {
+        //Du kan printe ut variabler med + utenfor "". Da trenger man ikke $ og {}
+        Console.WriteLine("Terje er kul. Verdien til i: "+ i);
+    }
+}
