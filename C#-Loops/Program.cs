d8d5694b882eec12c7ab14f290bf1f2e6cf5d897
Lonatri <gyda-94@hotmail.com>
2025-11-20 15:17:29 +0100

    While

--- C#-Loops/Program.cs
+++ C#-Loops/Program.cs
@@ -8,3 +8,34 @@ static void ForLoop()
         Console.WriteLine("Terje er kul. Verdien til i: "+ i);
     }
 }
+
+ForEachLoop();
+static void ForEachLoop()
+{
+    string text = "Some text";
+    foreach(var letter in text)
+    {
+        Console.WriteLine(letter);
+    }
+}
+
+WhileLoop();
+static void WhileLoop()
+{
+    bool condition = true;
+    int counter = 1;
+    int number = 4;
+    while (condition)
+    {
+        if(counter < 10)
+        {
+            Console.WriteLine($"Runde nr. {counter}\n" + number);
+            counter++;
+            number++;
+        }
+        else
+        {
+            condition = false;
+        }
+    }
+}
